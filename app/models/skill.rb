ef533aa81cd6149c532b2097263e41f0f6db92b5
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-02-25 06:45:27 +0100

    add some validates

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,7 @@
 class Skill < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
   has_many :skill_orders
   has_many :users, through: :skill_orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+
   has_many :skill_orders
   has_many :skills, through: :skill_orders
   # Include default devise modules. Others available are:
